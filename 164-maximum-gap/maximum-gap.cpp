90dfebb938df94e5329702dcc3f0ca77e7347a8f
jigyasarawat16-png <jigyasarawat16@gmail.com>
2026-09-22 20:18:34 +0530

    Time: 51 ms (19.04%) | Memory: 85.9 MB (74.97%) - LeetSync

---
+++ 164-maximum-gap/maximum-gap.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        if (nums.size() <= 1) {
+            return 0;
+        }
+
+        sort(nums.begin(), nums.end());
+
+        int maximum = 0;
+
+        for (int i = 1; i < nums.size(); i++) {
+            maximum = max(maximum, nums[i] - nums[i - 1]);
+        }
+
+        return maximum;
+    }
+};
